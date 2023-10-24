fd8580bf069109b272fa42c996dfbddc1683f096
Roger <54915190+Yuanjie-Xia@users.noreply.github.com>
2023-10-24 11:09:57 -0400

    create main and design function

---
+++ main.py
@@ -0,0 +1,38 @@
+class hybrid_workload:
+    def __init__(self):
+        pass
+
+    def load_workload(self):
+        pass
+
+    def load_performance(self):
+        pass
+
+    def select_lines(self):
+        pass
+
+    def merge_pattern(self):
+        pass
+
+    def get_start_time(self):
+        pass
+
+    def pair_performance_metrics(self):
+        pass
+
+    def set_window_size(self):
+        pass
+
+    def vectorize_signature(self):
+        pass
+
+
+if __name__ == "__main__":
+    hybrid_workload.load_workload()
+    hybrid_workload.vectorize_signature()
+    hybrid_workload.load_performance()
+    hybrid_workload.set_window_size()
+    hybrid_workload.get_start_time()
+    hybrid_workload.pair_performance_metrics()
+    hybrid_workload.merge_pattern()
+    hybrid_workload.pair_performance_metrics()
